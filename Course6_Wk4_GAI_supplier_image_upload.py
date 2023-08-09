3c4e280f23eaf9ba26d30075e177a4baece902ee
Kelyn Wong <kelynwonget@gmail.com>
2023-08-09 21:48:39 +0800

    Create Course6_Wk4_GAI_supplier_image_upload.py

---
+++ Course6_Wk4_GAI_supplier_image_upload.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+import os
+import requests
+
+def upload_images(image_dir, url):
+    for filename in os.listdir(image_dir):
+        if filename.endswith(".jpeg"):
+            image_path = os.path.join(image_dir, filename)
+            with open(image_path, 'rb') as opened:
+                response = requests.post(url, files={'file': opened})
+
+                if response.status_code == 201:
+                    print(f"Uploaded {filename} successfully")
+                else:
+                    print(f"Failed to upload {filename}, Status code: {response.status_code}")
+
+if name == "main":
+    image_directory = os.path.expanduser("~/supplier-data/images")
+    upload_url = "http://localhost/upload/"
+
+    upload_images(image_directory, upload_url)
